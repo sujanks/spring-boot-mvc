d6bdbf7cb308661d630945ab934ae7a21cc63946
Sujan Shrestha <sujankshrestha@gmail.com>
2017-01-05 14:25:34 +1100

    update

--- src/test/java/com/siras/ControllerTests.java
+++ src/test/java/com/siras/ControllerTests.java
@@ -12,11 +12,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by Sujan on 4/1/17.
@@ -33,11 +36,20 @@ public class ControllerTests {
 
     @Test
     public void testPersonGet() throws Exception {
-        given(this.personService.findById(1l))
-                .willReturn(new Person("sujan", "sujan@test", "abcd1234", "1231"));
+        List<Person> persons = new ArrayList<Person>();
+        persons.add(new Person("sujan", "sujan@test", "abcd1234", "1231"));
+        given(this.personService.findAll())
+                .willReturn(persons);
         mvc.perform(get("/person/")).andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(forwardedUrl("/WEB-INF/views/person/index.jsp"));
+                .andExpect(forwardedUrl("/WEB-INF/views/person/index.jsp"))
+                .andExpect(model().attribute("persons", hasSize(1)))
+                .andExpect(model().attribute("persons", hasItem(
+                        allOf(
+                                hasProperty("name", is("sujan")),
+                                hasProperty("email", is("sujan@test"))
+                        )
+                )));
 
     }
 }
